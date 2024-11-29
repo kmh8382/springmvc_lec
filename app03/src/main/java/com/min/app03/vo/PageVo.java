044610e56c4d530fa412d22e954f634c20ef5e83
kmh8382 <gorxhfm82@naver.com>
2024-11-29 18:03:42 +0900

    20241129

---
+++ app03/src/main/java/com/min/app03/vo/PageVo.java
@@ -0,0 +1,13 @@
+package com.min.app03.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class PageVo {
+  private String sort;
+  private int page;
+}
